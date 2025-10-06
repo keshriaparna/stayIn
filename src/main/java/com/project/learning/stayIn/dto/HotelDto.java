99b30279e85dab114bab3f0435f170369284f45f
Aparna Keshri <keshriaparna3@gmail.com>
2025-10-06 21:41:11 +0530

    Added HotelDto and RoomDto and updated room entity

---
+++ src/main/java/com/project/learning/stayIn/dto/HotelDto.java
@@ -0,0 +1,15 @@
+package com.project.learning.stayIn.dto;
+
+import com.project.learning.stayIn.entity.HotelContactInfo;
+import lombok.Data;
+
+@Data
+public class HotelDto {
+    private Long id;
+    private String name;
+    private String city;
+    private String[] photos;
+    private String[] amenities;
+    private HotelContactInfo contactInfo;
+    private Boolean active;
+}
